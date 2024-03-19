ba8889ea300cce93d2957c837fc9083d2083589e
Enrique Tecuapetla <enriquettip@gmail.com>
2024-03-18 20:52:02 -0600

    Crea encriptador sin primera vocal

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let listaNumeros=[];
+var texto="";
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -13,12 +15,54 @@ La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat" */
 
 function encriptador(){
-    let texto=document.getElementById('textoUsuario').value;
-    
-    console.log(texto);
-    console.log(typeof(texto));
-    console.log(texto.length);
+    texto=document.getElementById('textoUsuario').value;
+    var textoEncrip="";
+    let vocal=0;
+    for (let n=0;n<texto.length;n++ ){
+        if (texto[n]=="a" || texto[n]=="e" || texto[n]=="i" || texto[n]=="o" || texto[n]=="u"){
+            listaNumeros.push(n);
+        }else{
+            if(n+1==texto.length){
+                vocal=1;
+            }
+        }
+    }
+    let n=0,x=0;
+    while(n<listaNumeros.length){
+            if (listaNumeros[0]==0){
+                textoEncrip=comparacion(2,x);
+            }else{
+                textoEncrip+=comparacion(n,x);
+            }       
+        x=listaNumeros[n]+1;
+        n++;
+    }
+    if(vocal==1){
+        textoEncrip+=texto.substr(x,(texto.length-x));
+        console.log("cii");
+    }   
+    console.log(textoEncrip);
 }
 function desencriptador(){
 
+}
+function comparacion(n,x){
+    if(texto[listaNumeros[n]]=="a"){
+        return texto.substr(x,listaNumeros[n]-x)+"ai";
+     
+    }else if(texto[listaNumeros[n]]=="e"){
+        return texto.substr(x,listaNumeros[n]-x)+"enter";
+  
+    }
+    else if(texto[listaNumeros[n]]=="i"){
+        return texto.substr(x,listaNumeros[n]-x)+"imes";
+    }
+    else if(texto[listaNumeros[n]]=="o"){
+        return texto.substr(x,listaNumeros[n]-x)+"ober";
+    }
+    else if(texto[listaNumeros[n]]=="u"){
+        return texto.substr(x,listaNumeros[n]-x)+"ufat";
+       
+    }
+    return ;
 }
